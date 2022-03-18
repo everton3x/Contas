5ad12e0964e9a23dea06a02d146d77c034441333
Everton da Rosa <everton3x@gmail.com>
2022-03-18 14:12:46 -0300

    Corrigido bug do formato de data

--- public_html/receita_adicionar_salvar.php
+++ public_html/receita_adicionar_salvar.php
@@ -31,10 +31,10 @@ if (key_exists('valor', $_POST) && $_POST['valor'] > 0) {
     trigger_error("Valor não informado.", E_USER_ERROR);
 }
 
-if (key_exists('vencimento', $_POST)) {
+if (key_exists('vencimento', $_POST) && strlen($_POST['vencimento']) > 0) {
     $vencimento = $_POST['vencimento'];
 } else {
-    $vencimento = '';
+    $vencimento = '0000-00-00';
 }
 
 if (key_exists('agrupador', $_POST)) {
