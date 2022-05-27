9af9e7b1a38dc9ed01e7f542acd85c5ad6028295
Everton da Rosa <everton3x@gmail.com>
2022-05-27 15:55:02 -0300

    Remove o valor padrão da tela de a gastar

--- public_html/gastar.php
+++ public_html/gastar.php
@@ -42,7 +42,7 @@
             <legend class="title">Gasto</legend>
             <div class="field">
                 <label for="valor">Valor</label>
-                <input type="number" id="valor" name="valor" required min="0.01" step="0.01" value="<?=$agastar;?>" autofocus>
+                <input type="number" id="valor" name="valor" required min="0.01" step="0.01" autofocus>
             </div>
             <div class="field">
                 <label for="gasto">Gasto em</label>
